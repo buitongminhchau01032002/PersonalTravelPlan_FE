549730b96b712ab41cc6b794a501e5576646d315
Minh Chau <buitongminhchau01032002@gmail.com>
2023-07-19 08:41:50 +0700

    test input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RippleModule } from 'primeng/ripple';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RippleModule } from "primeng/ripple";
+import { InputTextModule } from "primeng/inputtext";
+import { InputNumberModule } from "primeng/inputnumber";
+import { InputTextareaModule } from "primeng/inputtextarea";
+import { DropdownModule } from "primeng/dropdown";
+import { MultiSelectModule } from "primeng/multiselect";
+import { CalendarModule } from "primeng/calendar";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { JourneyListComponent } from './journeys/journey-list/journey-list.component';
-import { JourneyCreateComponent } from './journeys/journey-create/journey-create.component';
-import { JourneyEditComponent } from './journeys/journey-edit/journey-edit.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { JourneyListComponent } from "./journeys/journey-list/journey-list.component";
+import { JourneyCreateComponent } from "./journeys/journey-create/journey-create.component";
+import { JourneyEditComponent } from "./journeys/journey-edit/journey-edit.component";
 
 @NgModule({
     declarations: [
@@ -19,7 +26,20 @@ import { JourneyEditComponent } from './journeys/journey-edit/journey-edit.compo
         JourneyCreateComponent,
         JourneyEditComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, ButtonModule, BrowserAnimationsModule, RippleModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        RippleModule,
+        ButtonModule,
+        InputTextModule,
+        InputNumberModule,
+        InputTextareaModule,
+        DropdownModule,
+        MultiSelectModule,
+        CalendarModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 primary: '#3699FF',
                 'green-light': '#1DC894',
                 bg: '#F5F5F5',
-                'bg-control': '#F5F8FA',
+                'bg-input': '#F5F8FA',
                 'status-planning': '#E8FFF3',
                 'status-finished': '#00B2FF',
                 'status-inprogess': '#FFF8DD',
