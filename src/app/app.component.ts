aa23d59341c8ce672c8d1e97268b3487873b0f45
Minh Chau <buitongminhchau01032002@gmail.com>
2023-07-25 15:10:42 +0700

    toggle change language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,16 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
     title = 'Personal Travel Plan';
+    languages: string[] = ['en', 'fr'];
+    currentLanguage: string = 'en';
+
     constructor(private primengConfig: PrimeNGConfig) {}
+
     ngOnInit() {
         this.primengConfig.ripple = true;
+    }
+
+    onLanguageChange(language: string) {
+        this.currentLanguage = language;
     }
 }
